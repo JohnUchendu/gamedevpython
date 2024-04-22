4a907b780366ef48841b0b971508b1e1bc77f554
John Uchendu <johnchnd195@gmail.com>
2024-04-22 14:24:55 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+
+# birth_year = int(input("Please enter your birth year: "))
+
+
+# age = 2024 - birth_year 
+
+# if age > 18: 
+#     print('you are an adult')
+# elif age == 0:
+#     print('you have not been born')
+# else:
+#     print('you are a child')
+
+
+# while loop
+
+# age = 33 
+
+# while age > 13:
+#     print('impossible')
+
+for num in range(30, 0, -3):
+    time.sleep(1)
+    print(num)
+print('Happy Mew Month')
+
