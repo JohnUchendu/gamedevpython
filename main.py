d847dfaa45943d7f09b3c8cde1af322e1d43c4a5
Abraham E. Hanson <ekenehanson@gmail.com>
2024-04-23 12:04:21 +0100

    reference text added

--- main.py
+++
@@ -1,7 +0,0 @@
-#List => It is a collection used to store multiple items in a varaiable
-#it is cretaed with square brackets
-
-#SET
-utensils = {"Spoon", "Fork", "Spoon", "Knife"}
-
-print(utensils)
